ee4af993165fd426e85a1c0e330bb572f62fab6f
baris-aydin <brsalpaydn@gmail.com>
2025-03-06 19:10:59 -0500

    last commit

---
+++ moveZeroes.py
@@ -0,0 +1,8 @@
+def moveZeroes(self, nums: List[int]) -> None:
+
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            nums.remove(nums[i])
+            nums.append(0)
+        else:
+            pass
